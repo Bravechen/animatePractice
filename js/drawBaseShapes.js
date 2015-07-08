116c44fa28cd6e4c9bca1549ab8a92cad86f24c8
Brave Chen <brave_cgx@163.com>
2015-07-08 14:54:36 +0800

    添加绘制圆形圆角曲线等例子

--- js/drawBaseShapes.js
+++ js/drawBaseShapes.js
@@ -19,6 +19,12 @@
      */
     function draw() {
         drawRect();
+        drawCircle();
+        drawSector(170,150,50,Math.PI*1.5);   //270度
+        drawRound(290,150,50);
+        drawBezier2();
+        drawBezier3();
+        useClip();
     }
     /**
      * 绘制矩形
@@ -57,6 +63,111 @@
         drawArea.fill();
         drawArea.stroke();
         drawArea.closePath();
+    }
+
+    /**
+     * 绘制圆形
+     */
+    function drawCircle(){
+        drawArea.beginPath();   //为了区别于上一个方法中的绘制路径
+        drawArea.lineWidth = 3;
+        drawArea.strokeStyle = '#86222a';
+        drawArea.arc(60,150,50,0,Math.PI*2,false);
+        drawArea.stroke();
+    }
+
+    /**
+     * 绘制扇形,使用arc和直线方法
+     * @param x {Number}
+     * @param y {Number}
+     * @param radius {Number} 半径
+     * @param angle {Number} 扇形角度，使用弧度单位
+     */
+    function drawSector(x,y,radius,angle){
+        drawArea.beginPath();
+        drawArea.lineWidth = 5;
+        drawArea.strokeStyle = '#b15971';
+        drawArea.moveTo(x,y);
+        drawArea.lineTo(x+radius,y);
+        drawArea.arc(x,y,radius,0,angle,false);
+        drawArea.lineTo(x,y);
+        drawArea.closePath();
+        drawArea.stroke();
+    }
+
+    /**
+     * 绘制圆角，使用arcTo和直线方法
+     * @param x {Number} 横坐标
+     * @param y {Number} 纵坐标
+     * @param radius {Number} 半径
+     */
+    function drawRound(x,y,radius){
+        drawArea.beginPath();
+        drawArea.lineWidth = 2;
+        drawArea.strokeStyle = "#0a0f25";
+        drawArea.moveTo(x,y);
+        drawArea.lineTo(x-radius,y);
+        drawArea.arcTo(x-radius,y-radius,x,y-radius,radius);
+        drawArea.lineTo(x,y);
+        drawArea.closePath();
+        drawArea.stroke();
+    }
+
+    /**
+     * 绘制二次贝塞尔曲线
+     */
+    function drawBezier2(){
+        drawArea.beginPath();
+        drawArea.strokeStyle = "#ba8a62";
+        drawArea.lineWidth = 3;
+        drawArea.moveTo(360,150);
+        drawArea.quadraticCurveTo(450,50,380,200);
+        drawArea.stroke();
+    }
+
+    /**
+     * 绘制三次贝塞尔曲线
+     */
+    function drawBezier3(){
+        drawArea.beginPath();
+        drawArea.strokeStyle = '#6c70f8';
+        drawArea.lineWidth = 3;
+        drawArea.moveTo(420,150);
+        drawArea.bezierCurveTo(500,10,400,170,430,220);
+        drawArea.stroke();
+    }
+
+    /**
+     * 使用clip绘制遮罩
+     */
+    function useClip(){
+
+        //应该有的区域
+        drawArea.beginPath();
+        drawArea.strokeStyle = "#ff0000";
+        drawArea.rect(100,300,200,100);
+        drawArea.rect(100,300,120,70);
+        drawArea.stroke();
+        drawArea.save();
+        //遮罩
+        drawArea.beginPath();
+        drawArea.arc(100,300,50,0,Math.PI*2,false);
+        drawArea.rect(120,320,100,50);
+        drawArea.clip();
+
+        //底部绘制的区域
+        drawArea.beginPath();
+        drawArea.fillStyle = "#432142";
+        drawArea.rect(100,300,200,100);
+        drawArea.fill();
+        drawArea.restore();
+
+        //save和restore之后不影响以后操作
+        drawArea.beginPath();
+        drawArea.strokeStyle = "#00ffff";
+        drawArea.rect(120,320,200,100);
+        drawArea.stroke();
+
     }
 
 })(window);
